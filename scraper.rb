d5cc423dd6123eef849e35122ca9ad4fbe502768
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-02-21 20:52:27 +1100

    Make scraper into a class so it can be tested

--- scraper.rb
+++ scraper.rb
@@ -5,39 +5,51 @@ $LOAD_PATH << "./lib"
 
 require "technology_one_scraper"
 
-def scrape(authorities)
-  exceptions = {}
-  authorities.each do |authority_label|
-    puts "\nCollecting feed data for #{authority_label}..."
+# Main Scraper class
+class Scraper
+  AUTHORITIES = TechnologyOneScraper::AUTHORITIES
 
-    begin
-      TechnologyOneScraper.scrape(authority_label) do |record|
-        record["authority_label"] = authority_label.to_s
-        TechnologyOneScraper.log(record)
-        ScraperWiki.save_sqlite(%w[authority_label council_reference], record)
+  def self.scrape(authorities)
+    exceptions = {}
+    authorities.each do |authority_label|
+      puts "\nCollecting feed data for #{authority_label}..."
+
+      begin
+        TechnologyOneScraper.scrape(authority_label) do |record|
+          record["authority_label"] = authority_label.to_s
+          TechnologyOneScraper.log(record)
+          ScraperWiki.save_sqlite(%w[authority_label council_reference], record)
+        end
+      rescue StandardError => e
+        warn "#{authority_label}: ERROR: #{e}"
+        warn e.backtrace
+        exceptions[authority_label] = e
       end
-    rescue StandardError => e
-      warn "#{authority_label}: ERROR: #{e}"
-      warn e.backtrace
-      exceptions[authority_label] = e
     end
+    exceptions
   end
-  exceptions
+
+  def self.selected_authorities
+    AUTHORITIES.keys
+  end
+
+  def self.run(authorities)
+    puts "Scraping authorities: #{authorities.join(', ')}"
+    exceptions = scrape(authorities)
+
+    unless exceptions.empty?
+      puts "\n***************************************************"
+      puts "Now retrying authorities which earlier had failures"
+      puts "***************************************************"
+
+      exceptions = scrape(exceptions.keys)
+    end
+
+    return if exceptions.empty?
+
+    raise "There were errors with the following authorities: #{exceptions.keys}. " \
+            "See earlier output for details"
+  end
 end
 
-authorities = TechnologyOneScraper::AUTHORITIES.keys
-puts "Scraping authorities: #{authorities.join(', ')}"
-exceptions = scrape(authorities)
-
-unless exceptions.empty?
-  puts "\n***************************************************"
-  puts "Now retrying authorities which earlier had failures"
-  puts "***************************************************"
-
-  exceptions = scrape(exceptions.keys)
-end
-
-unless exceptions.empty?
-  raise "There were errors with the following authorities: #{exceptions.keys}. " \
-        "See earlier output for details"
-end
+Scraper.run(Scraper.selected_authorities) if __FILE__ == $PROGRAM_NAME
